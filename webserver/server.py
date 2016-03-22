be1d6bd16df14c4e82128f57afdf3856b12bb239
RuiLu <Rui@Ruis-MacBook-Pro.local>
2016-03-21 20:37:20 -0400

    buy v6.2

--- webserver/server.py
+++ webserver/server.py
@@ -354,7 +354,7 @@ def order_status():
 @app.route('/product', methods=['GET', 'POST'])
 def product():
   error = None
-  if request.method == 'POST':
+  if request.method == 'GET':
     print 'heihei'
     search = request.form['search']
     minAmount = int(request.form['min'])
